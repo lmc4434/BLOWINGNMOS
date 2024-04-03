e63762b36036a974d1345de9a20a498c12881b02
lmc4434 <78935496+lmc4434@users.noreply.github.com>
2024-04-02 20:34:05 -0400

    Update camera.h

--- camera.h
+++ camera.h
@@ -1,5 +1,17 @@
-void myDelay(void);
+#ifndef CAMERA_H
+#define CAMERA_H
 
+#include <stdint.h>
+#include "Common.h" // Assuming BOOLEAN is defined in Common.h
+#define STR_SIZE 100
+#define CAMERA_DATA_SIZE 128
+
+// Declare variables related to camera functionality
+extern uint16_t line[CAMERA_DATA_SIZE];
+static char str[STR_SIZE];
+
+// Function prototypes for camera functionality
 void INIT_Camera(void);
+void camsequence(void);
 
-int cam_sequence(void);
+#endif /* CAMERA_H */
