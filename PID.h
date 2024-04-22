520e004f812f415c171be83989c930b05a4718ad
lmc4434 <78935496+lmc4434@users.noreply.github.com>
2024-04-22 09:04:24 -0400

    Create PID.h

---
+++ PID.h
@@ -0,0 +1,35 @@
+#include "msp.h"
+#include "uart.h"
+#include "timerA.h"
+#include "motors.h"
+#include "servo.h"
+#include "led.h"
+#include "controlpins.h"
+#include "adc.h"
+#include "timer32.h"
+#include "cortexm.h"
+#include "switches.h"
+#include "systicktimer.h"
+#include "camera.h"
+#include "oled.h"
+#include "TI_Main.h"
+#include <stdio.h>
+ 
+
+#ifndef PID_H
+#define PID_H
+
+#include "stdbool.h" // Include standard boolean library
+
+// Define constants
+#define Kp 1.0
+#define Ki 0.0
+#define Kd 0.0
+
+// Function declarations
+int find_center(void);
+float updateServoPosition(float control_signal);
+float PID(void);
+
+
+#endif /* PID_H */
