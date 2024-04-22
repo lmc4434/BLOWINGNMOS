55d1cc922494b624cb43455492901e2a35989578
lmc4434 <78935496+lmc4434@users.noreply.github.com>
2024-04-22 09:06:06 -0400

    Update TI_Main.c

--- TI_Main.c
+++ TI_Main.c
@@ -18,9 +18,11 @@
 #include "systicktimer.h"
 #include "camera.h"
 #include "oled.h"
+#include "TI_Main.h"
+#include "PID.h"
 #include <stdio.h>
 
-BOOLEAN debug = 0;
+
 
 extern unsigned char OLED_clr_data[1024];
 extern unsigned char OLED_TEXT_ARR[1024];
@@ -32,41 +34,11 @@ unsigned char state3[16] = "State 3";
 extern uint16_t line[128];
 extern char str[STR_SIZE];
 extern uint16_t SmoothData[128];
-extern uint16_t BinaryData[128];
 char temp[STR_SIZE];
 float test = 0.0;
+extern BOOLEAN center_flag;
+BOOLEAN debug = 0;
 
-// Global variables
-
-
-//PID SETUP
-float servo_position = 0.0;
-
-
-float integral = 0;
-float prev_error = 0;
-
-float Kp = 0.6; //0.6
-float Ki = 0.017; //0.15 //Large delay turn hard
-float Kd = 0.35; //0.20 //Return to center and turn hard
-BOOLEAN center_flag = 0;
-
-float updateServoPosition(float control_signal) {
-    
-    servo_position = control_signal;
-    if (servo_position <= 0.05) {
-        servo_position = 0.05;
-    } else if (servo_position >= 0.1) {
-        servo_position = 0.1;
-    }
-    // Return the updated servo position
-		if(debug){
-    uart2_put("Servo Position : ");
-		sprintf(temp,"%i\n\r", (int)(servo_position*1000));
-		uart2_put(temp);
-		}
-    return servo_position;
-}
 
 BOOLEAN carpet_detection() {
     if (line[64] < 6000){
@@ -76,31 +48,8 @@ BOOLEAN carpet_detection() {
 
 //END PID SETUP
 
-// Low Index
-int low_index = 50;
-
-// High Index
-int high_index = 78;
-
-char ctr[STR_SIZE];
-int switch_state = 0;
-double slope1 = 0;
-float tol = 0.01;
-
-float servo_current = 0.0;
-float servo_previous = 0.075;
-float s_err0 = 0.0;
-float s_err1 = 0.0;
-float s_err2 = 0.0;
-float s_err3 = 0.0;
-float s_err4 = 0.0;
-float s_err5 = 0.0;
-
-int switch1_state = 0;
-int switch2_state = 0;
 
 void init() {
-    // Init display
     uart2_init();
 		uart2_put("Bluetooth on");
     P3->SEL0 &= ~BIT6;
@@ -122,77 +71,7 @@ void init() {
     OLED_display_on();
 }
 
-int find_center(void){
-    BOOLEAN flag = FALSE;
-    int left_val;
-    int right_val;
-    for (int i = 0; i < 127; i++){
-        if (flag == FALSE){
-            if (BinaryData[i] == 1){
-                left_val = i;
-                flag = TRUE;
-                }
-    }else if (flag == TRUE){
-        if (BinaryData[i] == 0){
-            right_val = i;
-            flag = FALSE;
-        }
-    }
-	}
-  return (right_val + left_val)/2;
-}
 
-float PID(void){
-	/*
-	if (center > 63){
-		s_err0 = -1*(0.075 - ((float)center / 128) * (0.1 - 0.05) + 0.025);
-	} else {
-		s_err0 = 0.075 - ((float)center / 128) * (0.1 - 0.05) + 0.025;
-	}
-	*/
-	
-		s_err0 = 0.075 - (((find_center()) / 128.00) * (0.1) + 0.025);
-		if(debug){
-		uart2_put("SERR0: ");
-		sprintf(temp,"%f\n\r", s_err0*1000);
-		uart2_put(temp);
-		}
-		//if(s_err0 <= -0.064 || 0.190 >= s_err0){
-			servo_current = servo_previous + Kp*s_err0 + 
-				Ki*((s_err0+s_err1+s_err2+s_err3+s_err4+s_err5)/6) +
-				Kd*(s_err0 - s_err1);
-		//if (carpet_detection() != TRUE){
-				if(find_center() > 60 && find_center() < 68){
-		
-					servo_current = 0.075;
-					s_err0 = 0.0;
-					s_err1 = 0.0;
-					s_err2 = 0.0;
-					s_err3 = 0.0;
-					s_err4 = 0.0;
-					s_err5 = 0.0;
-			//}
-			center_flag = 1;
-					
-			servo_current = updateServoPosition(servo_current);
-			servo_previous = servo_current;
-			s_err5 = s_err4;
-			s_err4 = s_err3;
-			s_err3 = s_err2;
-			s_err2 = s_err1;
-			s_err1 = s_err0;
-		} else {
-			center_flag = 0;
-		}
-		//} 
-		
-				uart2_put("Servo Val: ");
-				sprintf(temp,"%f\n\r", servo_current);
-				uart2_put(temp);
-		
-		return servo_current;
-
-}
 
 void forward(float left_motor, float right_motor) {
     TIMER_A0_PWM_Init(4800, 0.0, 1);
@@ -217,44 +96,18 @@ void reverse_motors(float speed){
 
 float turn(float amount){
     TIMER_A2_PWM_DutyCycle(amount, 1); 
-	return amount;
+		return amount;
 }
 
-
-// Center of the graph should always be a slope of 0 if it's on the right course
-// so if the slope becomes + or -, have to turn the car
-double generate_slope(int y2, int y1, int x2, int x1) {
-    double result = (y2 - y1) / (x2 - x1);
-    return result;
-}
-
-// Figure out if it's a parabola - take index and value at a certain point and then the 
-// exact point reflected over the axis of symmetry. If it is a slope of 0 +- a tol
-// It is a parabola and it means it's on carpet
 
 int main(void) {
 
     init();
     delay(200);
-		/*while (switch2_state == 0){
-			if (switch1_state == 0 && ){
-				OLED_draw_line(1,1, state1);
-				OLED_write_display(OLED_TEXT_ARR);
-				delay(500);
-		}
-			if (Switch2_Pressed == TRUE){
-				switch2_state += 1;
-	}*/
 	
     forward(0.25,0.25);
     while(1) {
-        bin_enc();
-        //OLED_DisplayCameraData(SmoothData);
-				// Check This
-				//int square_wave_center = find_center();
-				//float error = setpoint - ((float)square_wave_center / 128) * (0.1 - 0.05) + 0.05;
-				
-				//End
+
 			test = turn(PID());
 			if(center_flag){
 				forward(0.50,0.50);
@@ -274,24 +127,9 @@ int main(void) {
 				sprintf(temp,"%i\n\r", find_center());
 				uart2_put(temp);
 			}
-        //if (carpet_detection() != TRUE) {
-            // This Needs to Turn a direction
-        //}
-				//else{
 					if(debug){
 					uart2_put("I EAT CARPET : ");
 					}
-					
-				//}
+
         }
     }
-
-/*
- * 
- * TIMER_A0_PWM_Init(4800, 0.0, 1);
- * TIMER_A0_PWM_Init(4800, 0.3, 2);
- * TIMER_A0_PWM_Init(4800, 0.0, 3);
- * TIMER_A0_PWM_Init(4800, 0.3, 4);
- * 
- * delay(1000);
- */
