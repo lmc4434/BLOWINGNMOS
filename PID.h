c1d265d972e37fa85a4c15558faa8e8d3174649e
gdb5215 <90352373+gdb5215@users.noreply.github.com>
2024-04-24 11:26:52 -0400

    Update PID.h

--- PID.h
+++ PID.h
@@ -1,8 +1,3 @@
-#include "msp.h"
-#include "uart.h"
-#include "timerA.h"
-#include "motors.h"
-#include "servo.h"
 #include "led.h"
 #include "controlpins.h"
 #include "adc.h"
@@ -12,7 +7,6 @@
 #include "systicktimer.h"
 #include "camera.h"
 #include "oled.h"
-#include "TI_Main.h"
 #include <stdio.h>
  
 
@@ -22,14 +16,13 @@
 #include "stdbool.h" // Include standard boolean library
 
 // Define constants
-#define Kp 0.045
-#define Ki 0.0005
-#define Kd 0.02
+
 
 // Function declarations
 int find_center(void);
 float updateServoPosition(float control_signal);
 float PID(void);
+float find_angle(float);
 
 
 #endif /* PID_H */
