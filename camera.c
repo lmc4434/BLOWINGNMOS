71c4f725ba962647eed367ddabc3e9a248c42bc5
gdb5215 <90352373+gdb5215@users.noreply.github.com>
2024-04-26 19:05:07 -0400

    Update camera.c

--- camera.c
+++ camera.c
@@ -25,7 +25,7 @@
 
 
 
-int bin_threshold = 13000; //15000
+int bin_threshold = 5300; //15000
 uint16_t SmoothData[128];
 uint16_t BinaryData[128];
 extern uint16_t line[128];
@@ -56,4 +56,3 @@ void bin_enc(void){
 		}
 	}
 }
-
